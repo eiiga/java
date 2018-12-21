2842cf981db62ef0f18d301e79d94bd5173a113e
五十嵐瑛 <ei.tktcwk.ud.f.603@gmail.com>
2018-12-22 02:19:20 +0900

    eclipse構築に伴うテストコミット

---
+++ test_java/src/test_java/test_hello.java
@@ -0,0 +1,21 @@
+package test_java;
+
+public class test_hello {
+
+	public static void main(String[] args) {
+//		ハローを表示
+		System.out.println("hello");
+
+//		変数に数字を代入
+		int num_test1 = 123;
+		int num_test2 = 456;
+		
+//		合計を算出
+		int total = num_test1 + num_test2;
+		System.out.println(total);
+		
+//		intと文字列の表示
+		System.out.print("合計：" + total);
+	}
+
+}
